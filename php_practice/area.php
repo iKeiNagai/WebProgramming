b6bf11a4f21e5029020bfd00fcd5921dbb9f8bbb
iKeiNagai <thomasvasquez68@gmail.com>
2025-02-27 14:02:22 -0500

    find rectangle area script

--- php_practice/area.php
+++ php_practice/area.php
@@ -3,7 +3,15 @@
 <body>
 
 <?php
-
+    $length = 5;
+    $width = 10;
+    
+    function rectangleArea($length, $width){
+        $area = $width * $length;
+        echo "$area";
+    }
+    
+    rectangleArea($length, $width);
 ?>
  
 </body>
