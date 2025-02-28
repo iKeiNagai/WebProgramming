029ebc12148e9746afa661f44be74454eeda9766
iKeiNagai <thomasvasquez68@gmail.com>
2025-02-28 18:08:59 -0500

    create triangle using nested loops php

---
+++ php_essentials/triangle.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+/*
+    creates triangle
+    *
+    **
+    ***
+    ****
+    *****
+*/
+
+for($x = 0; $x <= 5; $x++){
+    for($j = 0; $j <= $x; $j++){
+    	echo "*";
+    }
+    echo "<br>";
+}
+?> 
+
+</body>
+</html>
