9d90f1dd34d2b9946773b3516760e4a307da3d6f
Thomas <105030215+iKeiNagai@users.noreply.github.com>
2025-03-10 17:31:53 -0400

    data validation, it checks if any field is empty

--- form_php_2/sucker.php
+++ form_php_2/sucker.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>THANKS, sucker!</h1>
-
-<p>Your information has been recorded</p>
-
-<dl>
-    <dt>Name</dt>
-    <dd><?php print $_POST['name'] ?></dd>
-
-    <dt>Section</dt>
-    
-    <dd><?php print $_POST['section'] ?></dd>
-
-    <dt>Credit Card</dt>
-    <dd><?php print $_POST['cc'] ?> (<?php print $_POST['typecc'] ?>)</dd>
-</dl>
-
 <?php
     $name = $_POST['name'];
     $section = $_POST['section'];
     $cc = $_POST['cc'];
     $typecc = $_POST['typecc'];
 
-    $data = "$name;$section;$cc;$typecc" . PHP_EOL;
-    file_put_contents("suckers.html", $data, FILE_APPEND);  //set permissions 
-?>
+    if(!empty($name) and !empty($section) and !empty($cc) and !empty($typecc)){
+        echo "<h1>THANKS, sucker!</h1>";
 
-<pre>
-<?php 
-    $file_content = file_get_contents("suckers.html");
-    echo $file_content;
-?>
-</pre>
+        echo "<p>Your information has been recorded</p>";
 
+        echo "<dl>";
+
+        echo "<dt>Name</dt>";
+        echo "<dd> print". $name . "</dd>";
+
+        echo "<dt>Section</dt>";  
+        echo "<dd>". $section . "</dd>";
+
+        echo "<dt>Credit Card</dt>";
+        echo "<dd>" . $cc . "(" . $typecc .")</dd>";
+
+        echo "</dl>";
+
+        $data = "$name;$section;$cc;$typecc" . PHP_EOL;
+        file_put_contents("suckers.html", $data, FILE_APPEND);  //set permissions 
+
+        echo "<p>Here are all the suckers who have submitted here</p>";
+        echo "<pre>";
+
+        $file_content = file_get_contents("suckers.html");
+        echo $file_content;
+
+        echo "</pre>";
+
+    }else{
+        echo "<h1>Sorry</h1>";
+        echo "<p> You did not fill out the form completely.";
+        echo '<a href="buy_form.html">Try again?</a>';
+    }
+?>
 <?php  ?>
 
 </body>
