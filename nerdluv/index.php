b8fced542719211c58fa84c7079634b60f51e60e
Thomas <105030215+iKeiNagai@users.noreply.github.com>
2025-03-22 18:53:08 -0400

    html only

---
+++ nerdluv/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NerdLuv</title>
+</head>
+<body>
+    <header>
+        <h1>NerdLuv tm</h1>
+        <p>where meek geeks meet</p>
+    </header>
+    <nav>
+        <b>Welcome</b>
+        <ul>
+            <li><a href="">Sign up for a new account</a></li>
+            <li><a href="matches.php">Check your matches</a></li>
+        </ul>
+    </nav>
+    <footer>
+        <p>This page is for single nerds to meet and date each other! Type in <br>
+            your personal information and wait for the nerdly luv to begin!<br>
+            Thank you for using our site.<br>
+            <br>
+            Result and page (C) Copyright NerdLuv Inc.<br>
+        </p>
+    </footer>
+</body>
+</html>
