6c00e1fbbec58363841951954b42d1c69d2bbf63
Thomas <105030215+iKeiNagai@users.noreply.github.com>
2025-03-22 22:10:30 -0400

    html only

---
+++ nerdluv/matches-submit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nerdluv</title>
+</head>
+<body>
+    <header>
+        <h1>NerdLuv tm</h1>
+        <p>where meek geeks meet</p>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        <p>This page is for single nerds to meet and date each other! Type in <br>
+            your personal information and wait for the nerdly luv to begin!<br>
+            Thank you for using our site.<br>
+            <br>
+            Result and page (C) Copyright NerdLuv Inc.<br>
+        </p>
+    </footer>
+</body>
+</html>
