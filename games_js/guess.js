5a4fc2ef8fb74bf9799fc6574a83942fdfac3a54
Thomas <105030215+iKeiNagai@users.noreply.github.com>
2025-04-11 14:14:52 -0400

    timer logic

--- games_js/guess.js
+++ games_js/guess.js
@@ -1,29 +1,51 @@
 const guessInput = document.getElementById("guessInput");
 const guessButton = document.getElementById("guessButton");
 const feedback = document.getElementById("msg");
+const timerDisplay = document.getElementById("timer");
 
 //test variables
 const userNumber = document.getElementById("testNumber");
 const randomNumber = document.getElementById("testRand");
 
 let secretNumber;
+let startTime, timerInterval;
 
+//starts game
 function startGame(){
     secretNumber = Math.floor(Math.random() * 100) + 1;
+
+    startTime = Date.now();
+    updateTimer();
+    timerInterval = setInterval(updateTimer,1000);
+    
 }
 
+//endgame with message
 function endGame(message){
+    clearInterval(timerInterval); //stops timer
+
     feedback.textContent = message;
+
+    setTimeout(startGame, 2000); //new game after 2 secs
+
+}
+
+//updates timer
+function updateTimer(){
+    const elapsedSeconds = Math.floor((Date.now() - startTime) / 1000);
+    timerDisplay.textContent = `Time: ${elapsedSeconds}s`;
 }
 
 guessButton.addEventListener("click", function(){
     const guess = parseInt(guessInput.value.trim());
 
+    //input validation
     if(isNaN(guess) || guess < 1 || guess > 100){
         feedback.textContent = "Enter number between 1 and 100.";
         return;
     }
 
+    //if guess is correct
     if( guess === secretNumber){
         endGame(`Correct! The number was ${secretNumber}`);
     }else{
@@ -36,5 +58,5 @@ guessButton.addEventListener("click", function(){
 });
 
 
-
+//starts game
 window.addEventListener("DOMContentLoaded", startGame);
